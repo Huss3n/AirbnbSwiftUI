7fd70b4cfe3bee03d42bf6541307eb7cc7964876
Huss3n <aisackhussein@gmail.com>
2024-01-02 13:10:11 +0300

    Project setup

---
+++ AirbnbSwiftUI/App/AirbnbSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbSwiftUIApp.swift
+//  AirbnbSwiftUI
+//
+//  Created by Muktar Hussein on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
