74f397cc8cc8acab07ca419b418cd7705be37b5b
Huss3n <aisackhussein@gmail.com>
2024-01-02 16:29:57 +0300

    Completed Listing views.

---
+++ AirbnbSwiftUI/Components/Ratings.swift
@@ -0,0 +1,18 @@
+//
+//  Ratings.swift
+//  AirbnbSwiftUI
+//
+//  Created by Muktar Hussein on 02/01/2024.
+//
+
+import SwiftUI
+
+struct Ratings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Ratings()
+}
---
+++ AirbnbSwiftUI/Core/Listings/ListingDetails.swift
@@ -0,0 +1,18 @@
+//
+//  ListingDetails.swift
+//  AirbnbSwiftUI
+//
+//  Created by Muktar Hussein on 02/01/2024.
+//
+
+import SwiftUI
+
+struct ListingDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingDetails()
+}
